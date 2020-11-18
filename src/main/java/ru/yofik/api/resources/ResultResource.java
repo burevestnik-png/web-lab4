6ce387ca37ecb6775c5b28a1dd26005cb0b55f9a
Alexey <leshaego14@gmail.com>
2020-11-18 14:43:54 +0300

    Added using ResultService to ResultResource

--- src/main/java/ru/yofik/api/resources/ResultResource.java
+++ src/main/java/ru/yofik/api/resources/ResultResource.java
@@ -1,7 +1,12 @@
 package ru.yofik.api.resources;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.yofik.api.filters.SecuredResource;
+import ru.yofik.models.Result;
+import ru.yofik.models.resultService.ResultService;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -11,27 +16,37 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ResultResource {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+
+    @Inject
+    private ResultService resultService;
+
+
+
     @GET
-    public Response getAll() {
+    public Response getAll() throws JsonProcessingException {
         return Response
                 .ok()
-                .entity("This is get resultS")
+                .entity(OBJECT_MAPPER.writeValueAsString(resultService.getAll()))
                 .build();
     }
 
     @POST
-    public Response add() {
+    public Response add(final Result result) {
+        resultService.add(result);
+
         return Response
                 .ok()
-                .entity("This is add result")
                 .build();
     }
 
     @DELETE
     public Response clear() {
+        resultService.clear();
+
         return Response
                 .ok()
-                .entity("This is clear")
                 .build();
     }
 }
