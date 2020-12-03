08a43a9213426c3e7fb7829da88d3449c8f1ad60
Yaroslav <yaroslavsoldat2@gmail.com>
2020-12-03 18:40:55 +0300

    Mda

--- client/src/state/dot/actions.ts
+++ client/src/state/dot/actions.ts
@@ -1,9 +1,22 @@
-import { ADD_DOT } from '@state/svg/actionTypes'
+import { ADD_DOT, DOT_FAIL, DOT_SUCCESS } from '@state/dot/actionTypes'
 import Dot from '../../pages/CalculationPage/components/graph/svgElements/Dot'
 
 export const addDot = (dot: Dot): AddDotAction => {
     return {
         type: ADD_DOT,
         dot,
+    }
+}
+
+export const dotsSuccess = (): DotsSuccess => {
+    return {
+        type: DOT_SUCCESS,
+    }
+}
+
+export const dotsFail = (message: string | any): DotsFail => {
+    return {
+        type: DOT_FAIL,
+        message,
     }
 }
